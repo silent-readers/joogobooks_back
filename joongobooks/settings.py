55618800c4b1e64f34aaa28c0db74fbcaa3a4472
simseulnyang <happysseul627@gmail.com>
2023-08-19 01:21:08 +0900

    ✨ Feat : AUTH_USER_MODEL 및 DEFAULT_RENDERER_CLASSES 코드 작성

--- joongobooks/settings.py
+++ joongobooks/settings.py
@@ -19,6 +19,8 @@ from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Auth user
+AUTH_USER_MODEL = 'user.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -149,6 +151,9 @@ REST_FRAMEWORK = {
         # 'rest_framework.authentication.TokenAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
     ],
     'DEFAULT_PARSER_CLASSES': [  # request.data 속성에 엑세스 할 때 사용되는 파서 지정
         'rest_framework.parsers.JSONParser',
