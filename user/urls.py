c742edbaefa6422ac396d425a197faa7ce36816b
simseulnyang <happysseul627@gmail.com>
2023-08-20 00:22:32 +0900

    ✨ Feat : 회원가입, 로그인, token발행 관련 url 작성

---
+++ user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from user import views
+
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('register/', views.UserRegisterAPIView.as_view()),
+    path('auth/', views.AuthAPIView.as_view()),
+
+    # simplejwt 에서 제공하는 기본 JWT 인증
+    path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+]
