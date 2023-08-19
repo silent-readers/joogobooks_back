c9b9fe203373382f977a63e34a6ae99bf5b13516
simseulnyang <happysseul627@gmail.com>
2023-08-19 23:41:38 +0900

    ♻️ Refactor : project url 재설정

--- joongobooks/urls.py
+++ joongobooks/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('rest_framework.urls')),
+    # path('user/', include('user.urls')),
+    path('api/user/', include('user.urls')),
 ]
