48d50db10dbe5ae6052207714fe8e7f86e1fac46
shinyurim <103302201+yoursin0330@users.noreply.github.com>
2023-08-21 09:42:42 +0900

    ✨ Feat : Model, Serializer 추가

--- book/models.py
+++ book/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+class Post(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, required=True)
+    author = models.CharField(max_length=100, required=True)
+    publisher = models.CharField(max_length=100, required=True)
+    condition = models.CharField(max_length=10, required=True)
+    original_price = models.IntegerField(required=True, min_value=0)
+    selling_price = models.IntegerField(required=True, min_value=0)
+    detail_info = models.TextField(max_length=1000, required=True)
+    sale_condition = models.CharField(max_length=10, default="판매중")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class PostImage(models.Model):
+    postID = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="post")
+    image = models.ImageField(upload_to=None, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
---
+++ book/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Post, PostImage
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ('id', 'writer', 'title', 'author', 'publisher', 'condition', 'original_price', 'selling_price', 'detail_info', 'sale_condition', 'uploaded_at')
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImage
+        fields = ('id', 'postID', 'image', 'created_at')
