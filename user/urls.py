82da5389e959f03bad77efb2cdd983bb8284acc8
kim <k2h2j3@gmail.com>
2023-08-24 14:47:12 +0900

    ✨ profile update 기능구현, url연결

--- user/urls.py
+++ user/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('auth/', views.AuthAPIView.as_view()),
     path('profile/<int:user_id>/', views.ProfileView.as_view()),
     path('profile/<int:user_id>/create', views.ProfileCreateView.as_view()),
+    path('profile/<int:user_id>/update', views.ProfileUpdateView.as_view()),
 
     # simplejwt 에서 제공하는 기본 JWT 인증
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
