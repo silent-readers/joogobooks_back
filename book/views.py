a576b9507dee22c55e21544c6c1f74b8aaf35264
nekopurr <rksk0071@naver.com>
2023-08-24 18:32:53 +0900

    ✨ 상세보기, 수정, 삭제 뷰 기능 추가

--- book/views.py
+++ book/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, permissions
 from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from .models import Book
+from .serializers import BookSerializer
+from .forms import PostForm
 
 from .models import Book
 from .serializers import BookSerializer
@@ -28,3 +32,39 @@ class BookCreateView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+
+class BookDetailView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        serializer = BookSerializer(book)
+        return render(request, 'book/book_detail.html', {'book': serializer.data})
+
+
+class BookUpdateView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        form = PostForm(instance=book)
+        return render(request, 'book/book_edit.html', {'form': form, 'book': book})
+
+    def post(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        form = PostForm(request.POST, request.FILES, instance=book)
+        if form.is_valid():
+            new_book = form.save(commit=False)
+            new_book.writer = request.user
+            new_book.save()
+            return redirect('book_detail', book_id=new_book.id)
+        return render(request, 'book/book_edit.html', {'form': form, 'book': book, 'errors': form.errors})
+
+
+class BookDeleteView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def post(self, request, book_id):
+        book = get_object_or_404(Book, id=book_id)
+        book.delete()
+        return redirect('book_list')
