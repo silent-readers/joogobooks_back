78a42f77609c642682e42e7e83a1e58b3cf14ffe
simseulnyang <happysseul627@gmail.com>
2023-08-24 21:28:38 +0900

    🎨 Style : admin의 book list에 보여지는 부분 및 Meta 코드 수정

--- book/models.py
+++ book/models.py
@@ -14,14 +14,16 @@ class Book(models.Model):
     detail_info = models.TextField("상세내용", max_length=1000)
     sale_condition = models.CharField("판매상태", max_length=10, default="판매중")
     image = models.ImageField("이미지", upload_to='book_image/', blank=True)
+    
     SALE_CONDITION_CHOICES = [
         ("판매중", "판매중"),
         ("예약중", "예약중"),
         ("판매완료", "판매완료"),
     ]
+    
     uploaded_at = models.DateTimeField("작성시간",auto_now_add=True)
 
     sale_condition = models.CharField("판매 상태", max_length=10, choices=SALE_CONDITION_CHOICES)
 
     def __str__(self):
-        return self.title
+        return f"책 제목 : {self.title} / 책 저자 : {self.author} / 책 상태 : {self.condition} / 판매자: {self.writer} / 판매가격 {self.selling_price}"
--- book/serializers.py
+++ book/serializers.py
@@ -3,6 +3,7 @@ from .models import Book
 
 class BookSerializer(serializers.ModelSerializer):
     sale_condition_display = serializers.CharField(source='get_sale_condition_display', read_only=True)
+    
     class Meta:
         model = Book
-        fields = ('id', 'writer', 'title', 'author', 'publisher', 'condition', 'original_price', 'selling_price', 'detail_info', 'uploaded_at', 'image', 'sale_condition', 'sale_condition_display')
+        fields = '__all__'
