4973f6ba6c53bb86379b745bd74d1b86e767f5e8
nekopurr <rksk0071@naver.com>
2023-08-24 20:14:30 +0900

    🔥 Remove : Serializer로 교체

--- book/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Book
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['sale_condition', 'title', 'author', 'publisher', 'condition', 'original_price', 'selling_price', 'detail_info','image']
