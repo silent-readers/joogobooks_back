2bf97c8c978b6cee5433f182850838208a7ea699
simseulnyang <happysseul627@gmail.com>
2023-08-24 21:25:57 +0900

    🔥 Remove : 주석 및 사용하지 않는 코드 제거

--- book/views.py
+++ book/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
-from rest_framework.response import Response
 from rest_framework import status, permissions
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
+
 from .models import Book
 from .serializers import BookSerializer
 
