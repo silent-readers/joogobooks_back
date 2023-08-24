df0a9338e15a66d15033c440f81de6504aba5636
nekopurr <rksk0071@naver.com>
2023-08-24 20:23:25 +0900

    ♻️ Refactor : form -> serializer 교체, 인증 권한 수정

--- book/views.py
+++ book/views.py
@@ -5,12 +5,14 @@ from rest_framework.views import APIView
 from rest_framework.generics import get_object_or_404
 from .models import Book
 from .serializers import BookSerializer
-from .forms import PostForm
-
-from .models import Book
-from .serializers import BookSerializer
 
 # Create your views here.
+class IsBookAuthor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        book = view.get_object()
+        return request.user == book.user
+    
+
 class BookListView(APIView):
     
     def get(self, request):
@@ -39,33 +41,33 @@ class BookDetailView(APIView):
     def get(self, request, book_id):
         book = get_object_or_404(Book, id=book_id)
         serializer = BookSerializer(book)
-        return render(request, 'book/book_detail.html', {'book': serializer.data})
+        return Response(serializer.data)
 
 
 class BookUpdateView(APIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated, IsBookAuthor]
 
     def get(self, request, book_id):
         book = get_object_or_404(Book, id=book_id)
-        form = PostForm(instance=book)
-        return render(request, 'book/book_edit.html', {'form': form, 'book': book})
+        serializer = BookSerializer(book)
+        return Response(serializer.data)
 
-    def post(self, request, book_id):
+    def put(self, request, book_id):
         book = get_object_or_404(Book, id=book_id)
-        form = PostForm(request.POST, request.FILES, instance=book)
-        if form.is_valid():
-            new_book = form.save(commit=False)
-            new_book.writer = request.user
-            new_book.save()
-            return redirect('book_detail', book_id=new_book.id)
-        return render(request, 'book/book_edit.html', {'form': form, 'book': book, 'errors': form.errors})
+        serializer = BookSerializer(book, data=request.data)
+        if serializer.is_valid():
+            if request.user == book.user:
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response(status=status.HTTP_403_FORBIDDEN)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class BookDeleteView(APIView):
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [permissions.IsAuthenticated]
 
-    def post(self, request, book_id):
+    def delete(self, request, book_id):
         book = get_object_or_404(Book, id=book_id)
         book.delete()
-        return redirect('book_list')
-    
+        return Response(status=status.HTTP_204_NO_CONTENT)
