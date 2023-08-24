1c3ff5163e6689e4c1ab58dd041612390ecd40c7
nekopurr <rksk0071@naver.com>
2023-08-24 18:32:07 +0900

    ✨ Feat : 상세보기, 수정, 삭제 url 추가

--- book/urls.py
+++ book/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, include
-from .views import BookListView, BookCreateView
+from .views import BookListView, BookCreateView, BookDetailView, BookUpdateView, BookDeleteView
 
 
 urlpatterns = [
     path('list/', BookListView.as_view(), name='book-list'),
-    path('create/', BookCreateView.as_view(), name='book-create')
+    path('create/', BookCreateView.as_view(), name='book-create'),
+    path('<int:book_id>/', BookDetailView.as_view(), name='book-detail'),
+    path('<int:book_id>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('<int:book_id>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
