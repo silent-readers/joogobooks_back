81b21f82d728e356c0cbb6e4287dcaeab77c4af4
won0201 <114564396+won0201@users.noreply.github.com>
2023-08-25 09:11:35 +0900

    비밀번호 변경 및 회원 탈퇴 url 추가

--- user/urls.py
+++ user/urls.py
@@ -1,17 +1,18 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.urls import path, include
 from user import views
-
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from user import views
+from .views import UserRegisterAPIView, AuthAPIView, UserPasswordChangeAPIView, UserDeleteAPIView
 
 urlpatterns = [
     path('register/', views.UserRegisterAPIView.as_view()),
     path('auth/', views.AuthAPIView.as_view()),
     path('profile/<int:user_id>/', views.ProfileView.as_view()),
     path('profile/<int:user_id>/create', views.ProfileCreateView.as_view()),
+    path('password/change/', UserPasswordChangeAPIView.as_view(), name='user_password_change'),
+    path('delete/', UserDeleteAPIView.as_view(), name='user_delete'),
 
     # simplejwt 에서 제공하는 기본 JWT 인증
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
