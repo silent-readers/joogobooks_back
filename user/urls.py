1f617b9f332f61ee92ab572736b5cf7829d98cc1
won0201 <114564396+won0201@users.noreply.github.com>
2023-08-25 10:28:06 +0900

    import 중복 제거 및 password 보안 강화

--- user/urls.py
+++ user/urls.py
@@ -2,15 +2,14 @@ from django.urls import path, include
 from user import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from user import views
-from .views import UserRegisterAPIView, AuthAPIView, UserPasswordChangeAPIView, UserDeleteAPIView
 
 urlpatterns = [
     path('register/', views.UserRegisterAPIView.as_view()),
     path('auth/', views.AuthAPIView.as_view()),
     path('profile/<int:user_id>/', views.ProfileView.as_view()),
     path('profile/<int:user_id>/create', views.ProfileCreateView.as_view()),
-    path('password/change/', UserPasswordChangeAPIView.as_view(), name='user_password_change'),
-    path('delete/', UserDeleteAPIView.as_view(), name='user_delete'),
+    path('password/int:user_id/change/', views.UserPasswordChangeAPIView.as_view(), name='user_password_change'),
+    path('/int:user_id/delete/', views.UserDeleteAPIView.as_view(), name='user_delete'),
 
     # simplejwt 에서 제공하는 기본 JWT 인증
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
