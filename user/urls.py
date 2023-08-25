575adf21d17cb35ad845d47af08560f4000a4e83
simseulnyang <happysseul627@gmail.com>
2023-08-25 14:53:14 +0900

    ♻️ Refactor : url 코드 수정 및 정리

--- user/urls.py
+++ user/urls.py
@@ -6,13 +6,26 @@ from user import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    # 회원가입
     path('register/', views.UserRegisterAPIView.as_view()),
+    
+    # 로그인, 로그아웃, 회원정보 조회
     path('auth/', views.AuthAPIView.as_view()),
+
+    # 비밀번호 변경
+    path('auth/<int:user_id>/changepassword/', views.UserPasswordChangeAPIView.as_view()),
+
+    # 회원탈퇴
+    path('auth/<int:user_id>/delete/', views.UserDeleteAPIView.as_view()),
+
+    # 프로필 조회
     path('profile/<int:user_id>/', views.ProfileView.as_view()),
+
+    # 프로필 생성
     path('profile/<int:user_id>/create', views.ProfileCreateView.as_view()),
+
+    # 프로필 업데이트
     path('profile/<int:user_id>/update', views.ProfileUpdateView.as_view()),
-    path('password/<int:user_id>/change/', views.UserPasswordChangeAPIView.as_view(), name='user_password_change'),
-    path('<int:user_id>/delete/', views.UserDeleteAPIView.as_view(), name='user_delete'),
 
     # simplejwt 에서 제공하는 기본 JWT 인증
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
