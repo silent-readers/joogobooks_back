ada86b709ab5aa702c72a04312a116aba7219597
simseulnyang <happysseul627@gmail.com>
2023-08-30 14:44:26 +0900

    🎨 Style : 코드 정렬

--- book/models.py
+++ book/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Book(models.Model):
     writer = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField("도서명", max_length=100)
@@ -14,16 +15,20 @@ class Book(models.Model):
     detail_info = models.TextField("상세내용", max_length=1000)
     sale_condition = models.CharField("판매상태", max_length=10, default="판매중")
     image = models.ImageField("이미지", upload_to='book_image/', blank=True)
-    
+
+    class Meta:
+        ordering = ["-id"]
+
     SALE_CONDITION_CHOICES = [
         ("판매중", "판매중"),
         ("예약중", "예약중"),
         ("판매완료", "판매완료"),
     ]
-    
-    uploaded_at = models.DateTimeField("작성시간",auto_now_add=True)
 
-    sale_condition = models.CharField("판매 상태", max_length=10, choices=SALE_CONDITION_CHOICES)
+    uploaded_at = models.DateTimeField("작성시간", auto_now_add=True)
+
+    sale_condition = models.CharField(
+        "판매 상태", max_length=10, choices=SALE_CONDITION_CHOICES)
 
     def __str__(self):
         return f"책 제목 : {self.title} / 책 저자 : {self.author} / 책 상태 : {self.condition} / 판매자: {self.writer} / 판매가격 {self.selling_price}"
--- book/serializers.py
+++ book/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from .models import Book
 
+
 class BookSerializer(serializers.ModelSerializer):
-    sale_condition_display = serializers.CharField(source='get_sale_condition_display', read_only=True)
-    
+
+    sale_condition_display = serializers.CharField(
+        source='get_sale_condition_display', read_only=True)
+
     class Meta:
         model = Book
         fields = '__all__'
