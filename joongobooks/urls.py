f833eb4c8c97156a081382a1d6a5936fda497780
simseulnyang <happysseul627@gmail.com>
2023-09-02 10:17:14 +0900

    ✨ Feat : media 파일 사용을 위한 코드 추가

--- joongobooks/urls.py
+++ joongobooks/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
     path('book/', include('book.urls')),
     path('jjim/', include('jjim.urls')),
     path('api/chat/', include('chat.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
