c2c0cdc4052548b5660334bf723d9a98590fcb24
simseulnyang <happysseul627@gmail.com>
2023-09-03 16:42:40 +0900

    🐛 Fix : 사용하지 않는 기능 (channels)초기화 및 recommend 추가

--- joongobooks/asgi.py
+++ joongobooks/asgi.py
@@ -1,21 +1,16 @@
+"""
+ASGI config for chatbotproject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-import chat.routing
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatbotproject.settings')
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
-django_asgi_application = get_asgi_application()
-
-application = ProtocolTypeRouter(
-    {
-        "http": django_asgi_application,
-        "websocket": AuthMiddlewareStack(
-            URLRouter(
-                chat.routing.websocket_urlpatterns,
-            )
-        ),
-    }
-)
+application = get_asgi_application()
