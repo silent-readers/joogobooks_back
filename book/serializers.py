cfd93b7dea7586865c7342db5503eb157e7e60b4
simseulnyang <happysseul627@gmail.com>
2023-09-21 01:04:50 +0900

    🎨 style : uploaded_at format 설정

--- book/serializers.py
+++ book/serializers.py
@@ -3,12 +3,12 @@ from .models import Book, Comment, ChildComment
 
 
 class BookSerializer(serializers.ModelSerializer):
+    uploaded_at = serializers.DateTimeField(format="%Y년 %m월 %d일")
 
     sale_condition_display = serializers.CharField(
         source='get_sale_condition_display', read_only=True)
     writer_nickname = serializers.CharField(
         source='writer.nickname', read_only=True)
-    
 
     class Meta:
         model = Book
