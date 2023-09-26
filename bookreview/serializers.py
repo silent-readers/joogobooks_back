c09d2b91a56d3f1124335bd5c50ddcacb830bebe
simseulnyang <happysseul627@gmail.com>
2023-09-26 17:08:11 +0900

    ♻️ Refactor : 일부 Field 적용값 추가

--- bookreview/serializers.py
+++ bookreview/serializers.py
@@ -2,9 +2,13 @@ from rest_framework import serializers
 from .models import BookReview, BookReviewHashtag
 from book.models import Book
 
-### 서평
+
+# 서평
 class BookReviewListSerializer(serializers.ModelSerializer):
-    
+    created_at = serializers.DateTimeField(format="%Y.%m.%d")
+    writer_nickname = serializers.CharField(
+        source='review_writer.nickname', read_only=True)
+
     class Meta:
         model = BookReview
         exclude = ['review']
@@ -14,33 +18,41 @@ class BookReviewEditSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = BookReview
-        exclude =['review_writer']
+        exclude = ['review_writer']
         read_only_fields = ['view_count']
 
 
 class BookReviewSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(format="%Y년 %m월 %d일")
+    writer_nickname = serializers.CharField(
+        source='review_writer.nickname', read_only=True)
 
     class Meta:
         model = BookReview
         fields = '__all__'
         read_only_fields = ['view_count']
-        
-### 해시태그
+
+# 해시태그
+
+
 class BookReviewHashtagSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = BookReviewHashtag
         fields = '__all__'
+
 
 class BookReviewHashtagCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = BookReviewHashtag
         fields = '__all__'
-        read_only_fields = ['bookreview','writer']
-        
+        read_only_fields = ['bookreview', 'writer']
+
+
 class BookReviewHashtagSearchSerializer(serializers.ModelSerializer):
-    
+
     bookreview = BookReviewSerializer()
+
     class Meta:
         model = BookReviewHashtag
         fields = '__all__'
